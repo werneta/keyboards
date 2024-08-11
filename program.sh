9a79110595e3f99deb2aac205c676a18b89a5078
Thomas A. Werne <werneta@gmail.com>
2024-08-11 15:25:53 -0700

    Add countdown to model programmer

--- program.sh
+++ program.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-sleep 10
+for n in `seq 10 -1 1`; do
+    echo -n "\rProgramming in $n  "
+    sleep 1
+done
+
+echo "\r                        "
+echo "Programming"
 dfu-programmer atmega32u2 erase
 dfu-programmer atmega32u2 flash "$1"
 dfu-programmer atmega32u2 start
+echo "Done"
